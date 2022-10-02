030e9659c0a40d10a62c81df37aba3ecbcb31078
Mqoey <mqoeyyy@gmail.com>
2022-10-02 13:17:53 +0200

    fix bugs

--- Setmacmining/routes/web.php
+++ Setmacmining/routes/web.php
@@ -4,15 +4,14 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\SupportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::get('about', [AboutController::class, 'index'])->name('about');
-Route::get('service', function () {
-    return view('pages.service');
-})->name('service');
+Route::get('service', [ServiceController::class, 'index'])->name('service');
 Route::get('support', [SupportController::class, 'index'])->name('support');
 Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
 Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
